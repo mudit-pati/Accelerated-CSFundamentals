e79c64173356242589b2fdf698540b575c5bdb51
mudit-pati <muditpati@gmail.com>
2022-03-29 11:57:10 +0530

    The first C++ program for terminal execution

---
+++ Twosum.cpp
@@ -0,0 +1,16 @@
+/*
+Simple math.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5 ; // Initialising the first number as 5
+    int b = 2 ; // Initialising the second number as 2
+    a = a + 1 ; // Adding 1 to the first number
+    int res = a - b ; // Substracting the second number from above
+    cout << "The inputs are " << a << " and " << b << "." << endl;
+    cout << "The result is " << res <<".";
+}
